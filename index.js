2842c82daa571f0fb8f66fa6d3cb0140544e4829
Adewale Akano <kameelakano@gmail.com>
2024-07-16 22:53:59 +0000

    Timestamp code

--- index.js
+++ index.js
@@ -5,6 +5,50 @@
 var express = require('express');
 var app = express();
 
+const findTimestamp= async(req, res)=>{
+
+  const {date}= req.params;
+
+  const dateConstructor = new Date()
+
+if(!date){
+  dateInt = parseInt(date)
+     res.status(200).json({
+      unix: dateConstructor.getTime(), // Converts js timestamp to unix
+      utc: dateConstructor.toUTCString() // Gets 
+     }) 
+}
+
+else if(/^[0-9]*$/.test(Number(date))){
+ let newDate= Number(date)
+  res.status(200).json({
+   unix: newDate, 
+   utc: new Date(newDate).toUTCString()
+  }) 
+  }
+
+  else if(/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$/.test(date)){
+      res.status(200).json({
+       unix: new Date(date).getTime(), 
+       utc: new Date(date).toUTCString()
+      }) 
+      }
+
+    else if  (new Date(date) != "Invalid Date" ){
+      console.log(date);
+      res.status(200).json({
+        unix: new Date(date).getTime(), 
+        utc: new Date(date).toUTCString()
+       }) 
+      
+     }
+      else{ // Find out why the error has to be in  an else block
+      console.log(date);
+        res.status(400).json({ error : "Invalid Date" })
+      }
+}
+
+module.exports={findTimestamp}
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -14,9 +58,9 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get("/api", findTimestamp);
+
+app.get("/api/:date", findTimestamp);
 
 
 // your first API endpoint... 
